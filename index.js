83f894996baf9e130b029b9613fe3e7661339f4e
Max <max.liu0325@gmail.com>
2018-09-06 17:13:55 +0800

    fix

--- index.js
+++ index.js
@@ -16,22 +16,23 @@ function handleEvent(event) {
 				console.log('type:', type);
 				if (type == 0) return;
 				if (type == 3) {
-					return client.replyMessage(
-						event.replyToken,
-						{
-							type: 'datetimepicker',
-							label: 'Select date',
-							data: 'storeId=12345',
-							mode: 'datetime',
-							initial: '2017-12-25t00:00',
-							max: '2018-01-24t23:59',
-							min: '2017-12-25t00:00'
+					return client.replyMessage(replyToken, {
+						type: 'template',
+						altText: 'Datetime pickers alt text',
+						template: {
+							type: 'buttons',
+							text: 'Select date / time !',
+							actions: [
+								{ type: 'datetimepicker', label: 'date', data: 'DATE', mode: 'date' },
+								{ type: 'datetimepicker', label: 'time', data: 'TIME', mode: 'time' },
+								{ type: 'datetimepicker', label: 'datetime', data: 'DATETIME', mode: 'datetime' }
+							]
 						}
-						// {
-						// 	type: 'text',
-						// 	text: '請輸入 日期 時間 地點(Optional)\n2018/10/04 10:30 西門捷運站'
-						// }
-					);
+					});
+					// return client.replyMessage(event.replyToken, {
+					// 	type: 'text',
+					// 	text: '請輸入 日期 時間 地點(Optional)\n2018/10/04 10:30 西門捷運站'
+					// });
 				}
 			});
 	}
