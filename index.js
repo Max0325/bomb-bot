f029e0fa7a1b0b45f5732b4271412937c3880661
Max <max.liu0325@gmail.com>
2018-09-10 10:00:49 +0800

    fix

--- index.js
+++ index.js
@@ -1,32 +1,11 @@
 const line = require('@line/bot-sdk');
-const ParseServer = require('parse-server').ParseServer;
 const express = require('express');
-
-const databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 const lineConfig = {
 	channelAccessToken: process.env.HEROKU_LINE_CHANNEL_ACCESS_TOKEN,
 	channelSecret: process.env.HEROKU_LINE_CHANNEL_SECRET
 };
 const client = new line.Client(lineConfig);
 const app = express();
-
-if (!databaseUri) {
-	console.log('DATABASE_URI not specified, falling back to localhost.');
-}
-
-const api = new ParseServer({
-	databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
-	// cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-	appId: process.env.APP_ID || 'myAppId',
-	masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-	serverURL: process.env.SERVER_URL || 'https://spe3d-bot.herokuapp.com/parse', // Don't forget to change to https if needed
-	liveQuery: {
-		// classNames: [ 'Posts', 'Comments' ] // List of classes to support for query subscriptions
-	}
-});
-
-const mountPath = process.env.PARSE_MOUNT || '/parse';
-app.use(mountPath, api);
 
 const replyText = (token, texts) => {
 	texts = Array.isArray(texts) ? texts : [ texts ];
@@ -204,10 +183,6 @@ app.post('/', line.middleware(lineConfig), (req, res) => {
 });
 
 const port = process.env.PORT || 3000;
-const httpServer = require('https').createServer(app);
-httpServer.listen(port, function() {
+app.listen(port, () => {
 	console.log(`listening on ${port}`);
 });
-
-// This will enable the Live Query real-time server
-ParseServer.createLiveQueryServer(httpServer);
