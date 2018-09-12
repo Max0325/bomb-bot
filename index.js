15d0b1a2c701daf2abba9bd063c3d0336385c1ac
Max <max.liu0325@gmail.com>
2018-09-12 15:02:40 +0800

    fix

--- index.js
+++ index.js
@@ -145,6 +145,7 @@ async function handleText(info, message, replyToken, source) {
 			}
 			const bomb = await queryBomb.first();
 			bomb && (await bomb.save({ state: 'INVALID' }));
+			console.log(_.drop(cmds).join('T'), +moment(_.drop(cmds).join('T')));
 			await new Bomb().save({
 				timestamp: +moment(_.drop(cmds).join('T')),
 				owner: user,
