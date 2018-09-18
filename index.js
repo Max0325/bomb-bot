d83633962077b9a607b9dba630f9c596d783579c
Max <max.liu0325@gmail.com>
2018-09-18 19:17:46 +0800

    fix

--- index.js
+++ index.js
@@ -238,7 +238,9 @@ async function handleBomb(bomb) {
 	await client.pushMessage(key, {
 		type: 'bubble',
 		styles: {
-			footer: { separator: true }
+			footer: {
+				separator: true
+			}
 		},
 		body: {
 			type: 'box',
@@ -275,29 +277,146 @@ async function handleBomb(bomb) {
 					margin: 'xxl',
 					spacing: 'sm',
 					contents: [
-						..._(situations).filter('inactivate').orderBy('inactivate').map((situation) => ({
+						{
 							type: 'box',
 							layout: 'horizontal',
 							contents: [
 								{
 									type: 'text',
-									text: situation.player.displayName,
+									text: 'Energy Drink',
 									size: 'sm',
 									color: '#555555',
 									flex: 0
 								},
 								{
 									type: 'text',
-									text: moment(situation.inactivate).format('HH:mm:ss'),
+									text: '$2.99',
 									size: 'sm',
 									color: '#111111',
 									align: 'end'
 								}
 							]
-						})),
+						},
+						{
+							type: 'box',
+							layout: 'horizontal',
+							contents: [
+								{
+									type: 'text',
+									text: 'Chewing Gum',
+									size: 'sm',
+									color: '#555555',
+									flex: 0
+								},
+								{
+									type: 'text',
+									text: '$0.99',
+									size: 'sm',
+									color: '#111111',
+									align: 'end'
+								}
+							]
+						},
+						{
+							type: 'box',
+							layout: 'horizontal',
+							contents: [
+								{
+									type: 'text',
+									text: 'Bottled Water',
+									size: 'sm',
+									color: '#555555',
+									flex: 0
+								},
+								{
+									type: 'text',
+									text: '$3.33',
+									size: 'sm',
+									color: '#111111',
+									align: 'end'
+								}
+							]
+						},
 						{
 							type: 'separator',
 							margin: 'xxl'
+						},
+						{
+							type: 'box',
+							layout: 'horizontal',
+							margin: 'xxl',
+							contents: [
+								{
+									type: 'text',
+									text: 'ITEMS',
+									size: 'sm',
+									color: '#555555'
+								},
+								{
+									type: 'text',
+									text: '3',
+									size: 'sm',
+									color: '#111111',
+									align: 'end'
+								}
+							]
+						},
+						{
+							type: 'box',
+							layout: 'horizontal',
+							contents: [
+								{
+									type: 'text',
+									text: 'TOTAL',
+									size: 'sm',
+									color: '#555555'
+								},
+								{
+									type: 'text',
+									text: '$7.31',
+									size: 'sm',
+									color: '#111111',
+									align: 'end'
+								}
+							]
+						},
+						{
+							type: 'box',
+							layout: 'horizontal',
+							contents: [
+								{
+									type: 'text',
+									text: 'CASH',
+									size: 'sm',
+									color: '#555555'
+								},
+								{
+									type: 'text',
+									text: '$8.0',
+									size: 'sm',
+									color: '#111111',
+									align: 'end'
+								}
+							]
+						},
+						{
+							type: 'box',
+							layout: 'horizontal',
+							contents: [
+								{
+									type: 'text',
+									text: 'CHANGE',
+									size: 'sm',
+									color: '#555555'
+								},
+								{
+									type: 'text',
+									text: '$0.69',
+									size: 'sm',
+									color: '#111111',
+									align: 'end'
+								}
+							]
 						}
 					]
 				}
@@ -355,3 +474,73 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
 	console.log(`listening on ${port}`);
 });
+
+// {
+// 	type: 'bubble',
+// 	styles: {
+// 		footer: { separator: true }
+// 	},
+// 	body: {
+// 		type: 'box',
+// 		layout: 'vertical',
+// 		contents: [
+// 			{
+// 				type: 'text',
+// 				text: 'RECEIPT',
+// 				weight: 'bold',
+// 				color: '#1DB446',
+// 				size: 'sm'
+// 			},
+// 			{
+// 				type: 'text',
+// 				text: 'Brown Store',
+// 				weight: 'bold',
+// 				size: 'xxl',
+// 				margin: 'md'
+// 			},
+// 			{
+// 				type: 'text',
+// 				text: 'Miraina Tower, 4-1-6 Shinjuku, Tokyo',
+// 				size: 'xs',
+// 				color: '#aaaaaa',
+// 				wrap: true
+// 			},
+// 			{
+// 				type: 'separator',
+// 				margin: 'xxl'
+// 			},
+// 			{
+// 				type: 'box',
+// 				layout: 'vertical',
+// 				margin: 'xxl',
+// 				spacing: 'sm',
+// 				contents: [
+// 					..._(situations).filter('inactivate').orderBy('inactivate').map((situation) => ({
+// 						type: 'box',
+// 						layout: 'horizontal',
+// 						contents: [
+// 							{
+// 								type: 'text',
+// 								text: situation.player.displayName,
+// 								size: 'sm',
+// 								color: '#555555',
+// 								flex: 0
+// 							},
+// 							{
+// 								type: 'text',
+// 								text: moment(situation.inactivate).format('HH:mm:ss'),
+// 								size: 'sm',
+// 								color: '#111111',
+// 								align: 'end'
+// 							}
+// 						]
+// 					})),
+// 					{
+// 						type: 'separator',
+// 						margin: 'xxl'
+// 					}
+// 				]
+// 			}
+// 		]
+// 	}
+// }
